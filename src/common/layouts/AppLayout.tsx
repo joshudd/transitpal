07ca4379a91597bba10c8cae5913ec04e08fdd8c
Jacob <jacobschwantes@outlook.com>
2023-09-23 12:10:59 -0500

    feat: app layout

---
+++ src/common/layouts/AppLayout.tsx
@@ -0,0 +1,17 @@
+import { User } from "firebase/auth";
+import type { NextPage } from "next";
+import Header from "../components/Header";
+interface AppLayoutProps {
+  children: React.ReactNode;
+  user: User;
+  signOut: () => void;
+}
+const AppLayout: NextPage<AppLayoutProps> = ({ children, user, signOut }) => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Header user={user} signOut={signOut} />
+      {children}
+    </main>
+  );
+};
+export default AppLayout;
